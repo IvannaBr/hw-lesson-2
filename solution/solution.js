f5308ba8b29ec9bd8c509cb9502305f5875ff9a1
unknown <ivannalisenko2017@gmail.com>
2026-09-10 13:10:36 +0300

    add:hw-lesson-2

--- solution/solution.js
+++ solution/solution.js
@@ -11,8 +11,7 @@
 //   }
 // Запишіть результат як число або null (якщо нічого не виведеться)
 
-const task1Answer = null; // <- ваша відповідь
-
+const task1Answer = 1; // <- ваша відповідь
 
 // Задача №2
 // За допомогою циклу FOR виведіть рядки: "Пункт №1", "Пункт №2", ... "Пункт №10"
@@ -20,10 +19,13 @@ const task1Answer = null; // <- ваша відповідь
 
 function task2() {
   const result = [];
-  // ваш код тут
-
+  // ваш код тут 
+  for (let i = 1; i <= 10; i++) {
+    result.push(`Пункт №${i}`);
+  }
   return result;
 }
+console.log(task2());
 
 
 // Задача №3
@@ -31,6 +33,13 @@ function task2() {
 //   if (2 * 20 <= 10 || 30 / 2 < 5 && 10 <= "10" || 20 === "20") {
 //     console.log('000');
 //   }
+//if(40 <= 10 || 15<5   && 10 <= "10" || 20 === "20")
+//if(false || false &&  true || false)
+//if (false || false || false)
+//if (false || false )
+//if ()
+
+
 // Запишіть результат як рядок або null (якщо нічого не виведеться)
 
 const task3Answer = null; // <- ваша відповідь
@@ -42,7 +51,25 @@ const task3Answer = null; // <- ваша відповідь
 
 function divide(a, b) {
   // ваш код тут
+
+  const numberOne = Number(a);
+  const numberThoo = Number(b);
+
+  if(
+  Number.isNaN(numberOne) ||
+    Number.isNaN(numberThoo) ||
+    numberThoo === 0 ||
+    !Number.isFinite(numberOne / numberThoo))
+  {
+    return "Помилка: неможливо виконати ділення";
+  }
+
+  const result = numberOne / numberThoo;
+  return `Результат ділення: ${result}`;
 }
+
+console.log(divide(15, 2));
+
 
 
 // Задача №5
@@ -53,10 +80,25 @@ function divide(a, b) {
 
 function task5() {
   const arr = []; // <- ваш масив (має містити число 10)
-
   // ваш код перебору тут
-  // повертає true якщо знайдено 10, інакше false
+  for (let i = 0; i < 4; i++) {
+    const randomNum = Math.floor(Math.random() * 20) + 1;
+    arr.push(randomNum);
+  }
+  
+  arr.push(10);
+
+  console.log(arr.includes(10));
+  
+  return arr;
 }
+
+console.log(task5());
+
+// повертає true якщо знайдено 10, інакше false
+
+
+
 
 
 module.exports = { task1Answer, task2, task3Answer, divide, task5 };
